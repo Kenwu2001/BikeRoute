b2b2107fb5af3ddcbb6dce494cdcd01a0597d273
s109000114 <imeukg2001@gmail.com>
2025-06-07 17:45:11 +0800

    Update website static files

--- website/static/index.js
+++ website/static/index.js
@@ -4,7 +4,7 @@ const locationInfo = document.getElementById("location-info");
 const watchId = navigator.geolocation.watchPosition(function(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    locationInfo.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
+    locationInfo.textContent = `!!!!!!!!!!!!!!!!!!!!!!! Latitude: ${latitude}, Longitude: ${longitude}`;
 }, function(error) {
     locationInfo.textContent = `Error getting location: ${error.message}`;
 });
